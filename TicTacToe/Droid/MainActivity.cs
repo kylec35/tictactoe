fdaf8c5d2887d8fc5aea83109a3e91f1666edc75
kconway35 <kconway@nebraskaglobal.com>
2015-05-20 16:22:52 -0500

    Buttons present with working code

---
+++ TicTacToe/Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace tictactoe.Droid
+{
+	[Activity (Label = "tictactoe.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+
+			SetContentView (Resource.Layout.Main);
+
+			Button button = FindViewById<Button> (Resource.Id.button1);
+			button.Click += delegate{
+				button.Text = string.Format("X");
+			};
+
+		}
+	}
+}
+
